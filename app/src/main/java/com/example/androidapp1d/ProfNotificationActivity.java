b2b1e823f7fa8ecd41ab8dce8237f456b502dd6e
cheryl <cherylgohqp@users.noreply.github.com>
2017-12-05 13:37:04 +0800

    hi

---
+++ app/src/main/java/com/example/androidapp1d/ProfNotificationActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidapp1d;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profactivity_notification);
+    }
+}
