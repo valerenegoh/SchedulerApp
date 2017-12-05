10783e2d7bc467e89305701d0a71e2187ac687fd
WeirdVal <valerene_goh@mymail.sutd.edu.sg>
2017-12-05 18:36:24 +0800

    added stuff

--- app/src/main/java/com/example/androidapp1d/StudBookingActivity.java
+++ app/src/main/java/com/example/androidapp1d/StudBookingActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidapp1d;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
@@ -8,11 +9,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import java.util.ArrayList;
 
@@ -21,23 +23,79 @@ public class StudBookingActivity extends AppCompatActivity implements Navigation
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private ActionBar actionBar;
     private NavigationView navigationView;
+    private TabHost tabHost;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.studactivity_feed);
+        setContentView(R.layout.studactivity_booking);
 
         getSupportActionBar().setTitle("Your Bookings");
 
-        ListView lv =(ListView)findViewById(R.id.profList);
-        ArrayList<String> profList = new ArrayList<String>();
+        tabHost = (TabHost) findViewById(R.id.bookingsTab);
+        tabHost.setup();
+        TabHost.TabSpec profsTab = tabHost.newTabSpec("PROF TAB");
+        profsTab.setIndicator("PROF TAB");
+        profsTab.setContent(R.id.Professors);
+        tabHost.addTab(profsTab);
+
+        TabHost.TabSpec modsTab = tabHost.newTabSpec("MODS TAB");
+        modsTab.setIndicator("MODS TAB");
+        modsTab.setContent(R.id.Modules);
+        tabHost.addTab(modsTab);
+
+        //================================PROFESSORS TAB================================
+
+        ListView lv2 = (ListView)findViewById(R.id.professorsList);
+        ArrayList<String> profList = new ArrayList<>();
         profList.add("Oka Kurniawan");
-        profList.add("Valerene Goh");
-        profList.add("Cheryl Goh");
-        profList.add("Nigel Chan");
-        ArrayAdapter<String> lAdapter = new ArrayAdapter<String>(StudBookingActivity.this,android.R.layout.simple_list_item_1,profList);
-        lv.setAdapter(lAdapter);
+        profList.add("Ngai-Man Cheung");
+        profList.add("Jit Biswas");
+        profList.add("Norman Lee");
+        final ArrayAdapter<String> lAdapter2 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,profList);
+        lv2.setAdapter(lAdapter2);
+
+        SearchView searchProfs = (SearchView) findViewById(R.id.searchProfessors);
+        searchProfs.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                lAdapter2.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        //================================MODULES TAB================================
+
+        ListView lv1 = (ListView)findViewById(R.id.modulesList);
+        ArrayList<String> modList = new ArrayList<>();
+        modList.add("50.001 Introduction to Information Systems and Programming");
+        modList.add("50.002 Computational Structures");
+        modList.add("50.004 Introduction to Algorithms");
+        modList.add("02.105 Sages Through the Ages");
+        final ArrayAdapter<String> lAdapter1 = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,modList);
+        lv1.setAdapter(lAdapter1);
+
+        SearchView searchMods = (SearchView) findViewById(R.id.searchModules);
+        searchMods.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                lAdapter1.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        //=============================================================================
 
         drawerLayout =(DrawerLayout)findViewById(R.id.drawerLayout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.opendrawer, R.string.closedrawer);
